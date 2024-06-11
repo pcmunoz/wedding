5abfeb42abbae281f3cb4ae0eb42c6d7626edc23
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2024-06-11 22:34:07 +0800

    Add condition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,19 @@ const getReturnValues = (countDown: number) => {
 
 export default function Index() {
   const [days, hours, minutes, seconds] = useCountdown("07-05-2024");
-
+  const theDay = days < 0 && hours < 9 && minutes < 0 && seconds < 0;
   return (
     <main>
       <Container>
         <Image src="/images/20191009_110102.jpg" width={100} height={50} alt="us" />
         <Intro />
-        <h2 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8">Countdown</h2>
-        <p className="text-3xl">{`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds to go...`}</p>
+        {!theDay && (
+          <>
+            <h2 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8">Countdown</h2>
+
+            <p className="text-3xl">{`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds to go...`}</p>
+          </>
+        )}
         <Location />
       </Container>
     </main>
